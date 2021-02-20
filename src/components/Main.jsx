d849c49b7ffb82313b33ae336e6454b1cb760163
Stelian <stelian.tsankov@gmail.com>
2021-02-20 20:03:35 +0200

    final form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+//Do not remove Switch
 import { BrowserRouter as Switch, Route, Link } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Register from "./Pages/Register";
-//Do not remove Switch
 
 const Wrapper = styled.div``;
 
